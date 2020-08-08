05660a371a7fcb3a970b5e4337ae5437e30b779c
fabiano taddei <fabiano.taddei@gmail.com>
2020-08-08 21:02:58 +0200

    enlist, deepsort and delist implemented

--- nu.py
+++ nu.py
@@ -1,22 +1,57 @@
 #!/usr/bin/env python3
 
+TAB_CHAR   = '\t'
+POINT_CHAR = '-'
+
+def _get_indent(line):
+    line_len = len(line)
+    indent = 0
+    while indent <= line_len and line[indent] == TAB_CHAR:
+        indent += 1
+    return indent
+
 def enlist(lines_iter):
     """
     arrange lines in a recursive list of tuples (item, [sub-items-touples])
     """
-    return list()
+    result = list()
+    list_stack = [result, None]
+
+    indent = 0
+    for line in lines_iter:
+        l = []
+        t = (line, l)
+        line_indent = _get_indent(line)
+        if line_indent > indent:    # dealing with the first sub-item
+            list_stack.append(l)
+            list_stack[-2].append(t)
+        elif line_indent == indent: # same level
+            list_stack[-1] = l
+            list_stack[-2].append(t)
+        else:                       # 1+ lists to close
+            list_stack = list_stack[:(line_indent - indent - 1)]
+            list_stack.append(l)
+            list_stack[-2].append(t)
+        indent = line_indent
+    return result
 
 def deepsort(l):
     """
     Recursively sort in place each list
     """
-    return l
+    l.sort(key=lambda e:e[0])
+    for elem in l:
+        deepsort(elem[1])
 
-def delist(l):
+def delist(l, result=None):
     """
     returns touple of lines from the recursive list of tuples (item, [sub-items-touples])
     """
-    return ()
+    if not result: result = []
+    for line, sub in l:
+        result.append(line)
+        delist(sub, result=result)
+    return tuple(result)
 
 def main():
     ...
--- tests/test_nu.py
+++ tests/test_nu.py
@@ -1,8 +1,14 @@
 #!/usr/bin/env python3
 
 import unittest
-import nu
+
 from collections import namedtuple
+from copy import deepcopy
+
+import sys, os
+sys.path.insert(0, os.path.abspath(os.path.join(os.path.dirname(__file__), '..')))
+
+import nu
 
 TestLines = namedtuple("Testlines", ("name", "original", "enlisted",
                        "enlisted_sorted", "delisted"))
@@ -12,16 +18,45 @@ test_lines_list = (
     TestLines("simple",("s1", "s3", "s2"),
                 [("s1", list()), ("s3", list()), ("s2", list())],
                 [("s1", list()), ("s2", list()), ("s3", list())],
-            ("s1", "s3", "s2"))
+            ("s1", "s3", "s2")),
+    TestLines("sub-items",("s1", nu.TAB_CHAR + "ss4", nu.TAB_CHAR + "ss3", "s2"),
+                [("s1", [(nu.TAB_CHAR + "ss4", list()),
+                         (nu.TAB_CHAR + "ss3", list())]), ("s2", list())],
+                [("s1", [(nu.TAB_CHAR + "ss3", list()),
+                         (nu.TAB_CHAR + "ss4", list())]), ("s2", list())],
+            ("s1", nu.TAB_CHAR + "ss4", nu.TAB_CHAR + "ss3", "s2")),
+    TestLines("big-drop",("s1", nu.TAB_CHAR + "ss4", nu.TAB_CHAR *2 + "sss3",
+                          "s2"),
+                [("s1", [(nu.TAB_CHAR + "ss4", [(nu.TAB_CHAR *2 + "sss3",
+                                                list())])]), ("s2", list())],
+                [("s1", [(nu.TAB_CHAR + "ss4", [(nu.TAB_CHAR *2 + "sss3",
+                                                list())])]), ("s2", list())],
+            ("s1", nu.TAB_CHAR + "ss4", nu.TAB_CHAR *2 + "sss3", "s2"))
 )
 
-
 class TestNu(unittest.TestCase):
+
+    def test_helper_get_indent(self):
+        self.assertEqual(nu._get_indent("Zero"), 0)
+        self.assertEqual(nu._get_indent(nu.TAB_CHAR + "One"), 1)
+        self.assertEqual(nu._get_indent(nu.TAB_CHAR *2 + " - Two"), 2)
 
     def test_enlist(self):
         for tl in test_lines_list:
             er = f"failed to enlist {tl.name} TestLines"
             self.assertListEqual(nu.enlist(tl.original), tl.enlisted, er)
+
+    def test_deepsort(self):
+        for tl in test_lines_list:
+            tlc = deepcopy(tl)
+            nu.deepsort(tlc.enlisted)
+            er = f"failed to sort {tl.name} TestLines"
+            self.assertListEqual(tlc.enlisted, tl.enlisted_sorted, er)
+
+    def test_delist(self):
+        for tl in test_lines_list:
+            er = f"failed to delist {tl.name} TestLines"
+            self.assertEqual(nu.delist(tl.enlisted), tl.delisted, er)
 
 if __name__ == '__main__':
     unittest.main()
